d5a4791921da121a033b8dfabffd73edaf62f358
chrisrw <chrisrw@hungryfishmedia.com>
2014-03-03 11:10:51 -0500

    Filling in some initial data to work with

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,36 @@
 'use strict';
-require(['jquery', 'knockout'], function(jquery, ko) {
-	
-	// @todo - build actual models etc. ;)
-	var bandModel = function() {
-		this.bands = ko.observableArray(["The Faulty", "DDF", "Number 12"]);
-	};
+require(['jquery', 'knockout', 'underscore'], function(jquery, ko, underscore) {
 
-	ko.applyBindings(new bandModel());
+    console.log(underscore);
+    
+    var CollectionModel = function() {
+        this.cds = ko.observableArray([
+            { band: 'The Faulty',
+              title: 'The Kids are Ready',
+              tracks: ko.observableArray([
+                'one', 'two', 'three'
+            ]),
+              releaseDate: '2004',
+              price: null
+            },
+            { band: 'DDF',
+              title: 'Means to an End',
+              tracks: ko.observableArray([
+                'one', 'two', 'three'
+            ]),
+              releaseDate: '2001',
+              price: null
+            },
+            { band: 'Number 12',
+              title: 'Mongrel',
+              tracks: ko.observableArray([
+                'one', 'two', 'three'
+            ]),
+              releaseDate: '2007',
+              price: null
+            }
+        ]);
+    };
+
+    ko.applyBindings(new CollectionModel());
 });
